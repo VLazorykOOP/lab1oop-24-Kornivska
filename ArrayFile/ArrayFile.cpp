654a56adfef34866d73ada02b874368cea59250d
Аліна Корнівська <alinkornivska@gmail.com>
2024-02-10 21:27:38 +0200

    ввожу функцію для введення масиву з консолі

--- ArrayFile/ArrayFile.cpp
+++ ArrayFile/ArrayFile.cpp
@@ -1,3 +1,10 @@
 ﻿#include <iostream>
 #include <fstream>
 #include <vector>
+// Функція для введення масиву з консолі
+void inputArray(std::vector<int>& arr) {
+    std::cout << "Введіть " << arr.size() << " цілих чисел:\n";
+    for (int i = 0; i < arr.size(); ++i) {
+        std::cin >> arr[i];
+    }
+}
